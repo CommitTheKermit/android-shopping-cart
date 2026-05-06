527b66379316400f13c86955746834ee72c2948a
ujeong <qnf323@naver.com>
2026-05-06 20:33:07 +0900

    refactor: 프리뷰 어노테이션 추가

--- app/src/main/java/woowacourse/shopping/ui/productlist/LoadingIndicator.kt
+++ app/src/main/java/woowacourse/shopping/ui/productlist/LoadingIndicator.kt
@@ -10,8 +10,10 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
+@Preview
 fun LoadingIndicator(modifier: Modifier = Modifier) {
     Box(
         modifier = modifier
