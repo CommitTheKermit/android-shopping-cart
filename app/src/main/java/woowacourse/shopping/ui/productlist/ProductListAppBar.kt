05baa6e9e8f0349bd5bab62983e254bae4e40111
ujeong <qnf323@naver.com>
2026-05-07 13:34:19 +0900

    feat: 장바구니 수량 UI 구현

--- app/src/main/java/woowacourse/shopping/ui/productlist/ProductListAppBar.kt
+++ app/src/main/java/woowacourse/shopping/ui/productlist/ProductListAppBar.kt
@@ -1,6 +1,11 @@
 package woowacourse.shopping.ui.productlist
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -8,11 +13,14 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import woowacourse.shopping.R
@@ -33,12 +41,27 @@ fun ProductListAppBar(
             )
         },
         actions = {
-            IconButton(onClick = onCartIconClick) {
-                Icon(
-                    painter = painterResource(R.drawable.cart_icon),
-                    contentDescription = stringResource(R.string.cart_description),
-                    modifier = Modifier.padding(end = 10.dp),
-                )
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier.padding(end = 10.dp),
+            ) {
+                IconButton(
+                    onClick = onCartIconClick,
+                ) {
+                    Icon(
+                        painter = painterResource(R.drawable.cart_icon),
+                        contentDescription = stringResource(R.string.cart_description),
+                    )
+                }
+                Box(
+                    contentAlignment = Alignment.Center,
+                    modifier = Modifier
+                        .clip(CircleShape)
+                        .background(Color(0xff04c09e))
+                        .size(20.dp),
+                ) {
+                    Text("2", color = Color.White, fontSize = 14.sp)
+                }
             }
         },
         colors = TopAppBarDefaults.topAppBarColors(
@@ -48,4 +71,10 @@ fun ProductListAppBar(
         ),
         modifier = modifier,
     )
+}
+
+@Preview
+@Composable
+private fun ProductListAppBarPreview() {
+    ProductListAppBar({})
 }
