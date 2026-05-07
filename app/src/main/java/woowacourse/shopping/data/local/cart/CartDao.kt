a2c26bde4705669cedeacd444d489c590f89b41a
ujeong <qnf323@naver.com>
2026-05-07 17:41:19 +0900

    feat: 카트 Room 추가

---
+++ app/src/main/java/woowacourse/shopping/data/local/cart/CartDao.kt
@@ -0,0 +1,24 @@
+package woowacourse.shopping.data.local.cart
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+
+@Dao
+interface CartDao {
+    @Query("SELECT * FROM cart_items")
+    suspend fun findAll(): List<CartItemEntity>
+
+    @Query("SELECT * FROM cart_items WHERE productId = :id")
+    suspend fun findById(id: String): CartItemEntity?
+
+    @Insert
+    suspend fun insert(item: CartItemEntity)
+
+    @Update
+    suspend fun update(item: CartItemEntity)
+
+    @Query("DELETE FROM cart_items WHERE productId = :id")
+    suspend fun deleteById(id: String)
+}
