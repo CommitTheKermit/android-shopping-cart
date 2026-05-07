46c1ea454a9e557fd7f0f90124a4ad8cf7a48adc
ujeong <qnf323@naver.com>
2026-05-07 13:33:45 +0900

    feat: 수량 버튼 UI 구현

--- app/src/main/java/woowacourse/shopping/ui/productlist/ProductItem.kt
+++ app/src/main/java/woowacourse/shopping/ui/productlist/ProductItem.kt
@@ -1,21 +1,34 @@
 package woowacourse.shopping.ui.productlist
 
+import android.R.attr.bottom
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import woowacourse.shopping.R
+import woowacourse.shopping.ui.common.ProductQuantitySelector
 
 @Composable
 fun ProductItem(
@@ -25,11 +38,21 @@ fun ProductItem(
     modifier: Modifier = Modifier,
 ) {
     Column(modifier = modifier.fillMaxWidth()) {
-        PreviewableAsyncImage(
-            imageUrl = imageUrl,
-            description = name,
-            modifier = Modifier.aspectRatio(1f),
-        )
+        Box {
+            PreviewableAsyncImage(
+                imageUrl = imageUrl,
+                description = name,
+                modifier = Modifier.aspectRatio(1f),
+            )
+//            ProductInitialAddButton(modifier = Modifier.align(Alignment.BottomEnd).padding(bottom = 4.dp, end = 4.dp))
+            ProductQuantitySelector(
+                modifier = Modifier
+                    .height(42.dp)
+                    .fillMaxWidth()
+                    .align(Alignment.BottomEnd)
+                    .padding(bottom = 6.dp, start = 4.dp, end = 4.dp),
+            )
+        }
         Spacer(modifier = Modifier.height(9.dp))
         Text(
             text = name,
@@ -61,4 +84,23 @@ private fun PreviewProduct() {
             .width(160.dp)
             .padding(horizontal = 16.dp),
     )
+}
+
+@Composable
+@Preview
+fun ProductInitialAddButton(modifier: Modifier = Modifier) {
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = modifier
+            .size(48.dp)
+            .clip(CircleShape)
+            .background(Color.White),
+    ) {
+        Icon(
+            imageVector = Icons.Default.Add,
+            contentDescription = stringResource(R.string.add_product_description),
+            modifier = Modifier.size(40.dp),
+            tint = Color(0xff555555),
+        )
+    }
 }
